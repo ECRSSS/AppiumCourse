5d5d2367e11272aaf21aad0b6570eeeb52f43e12
ECRSSS <n.n.glebanov@gmail.com>
2019-05-02 15:51:42 +0300

    Test 2

--- src/MainClassTest.java
+++ src/MainClassTest.java
@@ -3,7 +3,12 @@ import org.junit.Test;
 
 public class MainClassTest {
     @Test
-    public void assertNumberTest(){
-        Assert.assertTrue("Результат вычисления не равен 14",new MainClass().getLocalNumber() == 14);
+    public void assertLocalNumberTest() {
+        Assert.assertTrue("Результат вычисления не равен 14", new MainClass().getLocalNumber() == 14);
+    }
+
+    @Test
+    public void assertClassNumberTest() {
+        Assert.assertTrue("Результат вычисления не больше 45", new MainClass().getClassNumber() > 45);
     }
 }
